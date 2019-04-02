b8e615f3680618cc444f1e2f729b886cac28e66e
HelioStrike <rageofepicfury@gmail.com>
2019-04-02 12:58:08 +0530

    added login functionality

--- ldap_client/index.js
+++ ldap_client/index.js
@@ -1,9 +1,26 @@
-var ldap = require('ldapjs');
+var express           = require('express'),
+    app               = express(),
+    bodyParser        = require('body-parser'),
+    ldap = require('ldapjs');
 
 var client = ldap.createClient({
   url: 'ldap://127.0.0.1:1389'
 });
 
-client.bind('cn=root', 'secret', function(err) {
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get('/login', function(req, res) {
+  res.render("login");
+});
+
+app.post('/login', function(req, res) {
+  client.bind('cn='+req.body.username, req.body.password, function(err) {
     console.log(err);
+    if(!err) res.send("Success!!!");
+  });
+});
+
+app.listen(3001, '0.0.0.0', function() {
+  console.log("App started on localhost:3001");
 });
